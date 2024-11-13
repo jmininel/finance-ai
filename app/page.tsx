215fd7da1a1e5351a9e21059b3a4055c9248e2fb
jmininel <ju.mininel@hotmail.com>
2024-11-13 15:09:00 -0300

    chore: setup prisma and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "./_components/ui/button";
+
+const Home = () => {
+  return <Button>ok</Button>;
+};
+
+export default Home;
---
+++ app/subscription/page.tsx
@@ -0,0 +1,7 @@
+/** @format */
+
+const Subscription = () => {
+  return <h1>Subscription</h1>;
+};
+
+export default Subscription;
